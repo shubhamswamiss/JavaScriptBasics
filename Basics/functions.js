3d0c2828077efdbb61de20f324d32c7015f75585
Shubham <shubhamswamiss333@gmail.com>
2023-12-20 13:09:01 +0530

    functions

--- Basics/functions.js
+++ Basics/functions.js
@@ -1,56 +1,70 @@
+
+function sayMyName(){
+   console.log("H");
+   console.log("I");
+   console.log("T");
+   console.log("E");
+   console.log("S");
+   console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+   // let result = number1 + number2
+   // return result
+   return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
 function loginUserMessage(username = "sam"){
    if(!username){
-        console.log(" Please Enter Username ");
-        return
+       console.log("PLease enter a username");
+       return
    }
    return `${username} just logged in`
 }
 
-console.log(loginUserMessage('hitesh'));
-console.log(loginUserMessage('Sakshi'));
-
-// Shopping cart situation - When user adding n no. of products into cart then we can use 
-//  rest operator
- function calculateCartPrice(...num){
-    return num;
- }
-
- console.log(calculateCartPrice(200,300,400,500,800,600))
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
 
 
- function anotherCartEx(val1,val2,...num){
-      return num;
- }
-
- console.log(anotherCartEx(235,64,9764,94644,546,'addtocart'))
-
- // Passing Object as function argument 
-
- const usr = {
-    productName: 'Computer',
-    price: 50000
- }
-
- function handleObject(anyObject){
-    console.log(`Product is ${anyObject.productName} and Price is ${anyObject.price}`);
- }
-
- handleObject(usr);
- handleObject({
-    productName : 'ERP',
-    price: 1500000
- });
-
- // Passing Array as an argument
-
- const newArr = [100,20,36,165,564,8,7];
-
-function returnarrayValue(getArray){
-    return getArray;
+function calculateCartPrice(val1, val2, ...num1){
+   return num1
 }
-console.log(returnarrayValue(newArr));
-console.log(returnarrayValue([1,5,4,86,]))
 
+// console.log(calculateCartPrice(200, 400, 500, 2000))
 
+const user = {
+   username: "hitesh",
+   prices: 199
+}
 
+function handleObject(anyobject){
+   console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
 
+// handleObject(user)
+handleObject({
+   username: "sam",
+   price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+   return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
