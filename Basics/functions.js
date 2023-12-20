5a35aafccbe90a1479beb74fffde80541de48e28
Shubham <shubhamswamiss333@gmail.com>
2023-12-20 16:00:39 +0530

    functions

--- Basics/functions.js
+++ Basics/functions.js
@@ -1,4 +1,13 @@
+function loginUserMessage(username = "sam"){
+   if(!username){
+        console.log(" Please Enter Username ");
+        return
+   }
+   return `${username} just logged in`
+}
 
+console.log(loginUserMessage('hitesh'))
+console.log(loginUserMessage('Sakshi'))
 function sayMyName(){
    console.log("H");
    console.log("I");
