a6b5415839bf7ff851181df02cc782ac44515ead
shubhamswamiss <shubhamswamiss333@gmail.com>
2024-02-15 13:19:30 +0530

    changing PI property

---
+++ Basics/mathPI.js
@@ -0,0 +1,36 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math,'PI')
+console.log(descriptor);  /* we cannot change the property of pi {
+    value: 3.141592653589793,
+    writable: false,
+    enumerable: false,
+    configurable: false
+  } */
+
+  console.log(Math.PI);
+  Math.PI = 5;
+  console.log(Math.PI);
+
+  const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log('chai not done');
+    }
+  }
+
+  console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+  Object.defineProperty(chai,'name',{
+    // writable: false,
+       enumerable: false
+  })
+
+  console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+  for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+     console.log(`${key} : ${value}`);
+    }
+  }
