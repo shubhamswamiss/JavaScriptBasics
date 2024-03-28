eb53afc1b3da59ce480e377bc2432e7aa74b7094
shubhamswamiss <shubhamswamiss333@gmail.com>
2024-03-28 12:37:08 +0530

    callback function

---
+++ Basics/callbackFun.js
@@ -0,0 +1,16 @@
+function fetchData(callback) {
+  const data1 = "dsafas";
+  console.log(callback(data1));
+  // fetching data from server
+  setTimeout(() => {
+    const data = "This is some data from server";
+    // Call the callback function and pass the data to it
+    callback(data);
+  }, 3000);
+}
+
+function processData(data) {
+  console.log("Data received:", data);
+}
+
+fetchData(processData);
